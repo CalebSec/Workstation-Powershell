cf8795cb29b7855c887a579b0833a03c94a4fa94
Caleb Leggett <153628730+CalebSec@users.noreply.github.com>
2024-09-30 08:00:57 -0500

    Update SW Deployment.ps1

--- SW Deployment.ps1
+++ SW Deployment.ps1
@@ -1,3 +1,9 @@
+#This is a Chocolatey Software deployment for common software
+#Prerequisite for this is to install the Chocolatey repository
+
+#Setting the Execution Policy, setting the security protocol, and installing the repository
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 choco install malwarebytes; 
 choco install googleearth; 
 choco install greenshot; 
